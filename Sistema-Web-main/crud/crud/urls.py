e4a459c88d3f9f8e697d004ea4eed19f2906d1eb
igor-miranda <igor.miranda@ufvjm.edu.br>
2023-09-28 12:56:18 -0300

    up

--- Sistema-Web-main/crud/crud/urls.py
+++ Sistema-Web-main/crud/crud/urls.py
@@ -5,7 +5,6 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('', RedirectView.as_view(url='/home/login', permanent=False)),
     path('admin/', admin.site.urls),
-    path('historico/', include('historico.urls')),  
     path('cadastro/', include('cadastro.urls')),
     path('home/', include('home.urls')),
 ]
