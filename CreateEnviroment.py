6c8576779cb98f83aa43413f2a900fad45562928
Walter <waltermagno02@gmail.com>
2023-10-22 15:23:53 -0300

    Ajustes

--- CreateEnviroment.py
+++ CreateEnviroment.py
@@ -1,34 +1,33 @@
 import os
 import subprocess
 
-# Nome do ambiente virtual
 nome_ambiente = "env"
 
-# Verifica se o ambiente virtual já existe
-if not os.path.isdir(nome_ambiente):
-    # Cria o ambiente virtual
-    subprocess.call(["python", "-m", "venv", nome_ambiente])
-
-# Atualiza o pip e instala as dependências
 if os.name == 'nt':  # Windows
+    if not os.path.isdir(nome_ambiente):
+        subprocess.call(["python", "-m", "venv", nome_ambiente])
+
     subprocess.call([f"{nome_ambiente}\\Scripts\\pip", "install", "--upgrade", "pip"])
     subprocess.call([f"{nome_ambiente}\\Scripts\\pip", "install", "-r", "requirements.txt"])
+
 else:  # macOS e Linux
+    if not os.path.isdir(nome_ambiente):
+        subprocess.call(["python3", "-m", "venv", nome_ambiente])
+
     subprocess.call([f"{nome_ambiente}/bin/pip", "install", "--upgrade", "pip"])
     subprocess.call([f"{nome_ambiente}/bin/pip", "install", "-r", "requirements.txt"])
 
 
-# Comando para ativar o ambiente virtual
 comando_ativacao = ""
 
 if os.name == 'nt':  # Windows
     comando_ativacao = f".\\{nome_ambiente}\\Scripts\\activate"
-    print(f"Para ativar o ambiente: {comando_ativacao}")
-else:  # macOS e Linux
-    comando_ativacao = f"source ./{nome_ambiente}/bin/activate"
+    ubprocess.run(comando_ativacao, shell=True)
     print(f"Para ativar o ambiente: {comando_ativacao}")
 
-# Ativa o ambiente virtual
-#subprocess.run(comando_ativacao, shell=True)
+else:  # macOS e Linux
+    comando_ativacao = f"source ./{nome_ambiente}/bin/activate"
+    subprocess.run(comando_ativacao, shell=True)
+    print(f"Para ativar o ambiente: {comando_ativacao}")
 
 
--- UpdateRequirements.py
+++ UpdateRequirements.py
@@ -7,7 +7,10 @@ def atualizar_requirements(nome_ambiente):
     print("Atualizando o arquivo requirements.txt usando o pip do ambiente virtual.")
     
     pip_path = os.path.join(nome_ambiente, 'Scripts', 'pip') if os.name == 'nt' else os.path.join(nome_ambiente, 'bin', 'pip')
-    subprocess.call([pip_path, "freeze", ">", "requirements.txt"], shell=True)
+    
+    # Usar subprocess.run para atualizar o arquivo requirements.txt
+    with open("requirements.txt", "w") as requirements_file:
+        subprocess.run([pip_path, "freeze"], stdout=requirements_file, text=True)
 
 def deletar_ambiente_virtual(nome_ambiente):
     """Apaga o ambiente virtual."""
@@ -16,15 +19,11 @@ def deletar_ambiente_virtual(nome_ambiente):
     else:  # macOS e Linux
         subprocess.call(["rm", "-rf", nome_ambiente])
 
-# Diretório do ambiente virtual (na raiz do projeto)
 nome_ambiente = "env"
 
-# Verifica se o ambiente virtual existe
 if os.path.exists(nome_ambiente):
-    # Atualizar o arquivo requirements.txt
     atualizar_requirements(nome_ambiente)
 
-    # Pergunta ao usuário se deseja apagar o ambiente virtual
     '''resposta = input(f"Arquivo requirements atualizado! Deseja apagar o ambiente virtual '{nome_ambiente}'? [s] ou [n]: ").lower()
 
     if resposta == 's':
@@ -33,4 +32,4 @@ if os.path.exists(nome_ambiente):
     else:
         print(f"Ambiente virtual '{nome_ambiente}' foi mantido.")'''
 else:
-    print(f"Não foi possivel atualizar o arquivo requirements, ambiente virtual '{nome_ambiente}' não encontrado na raiz do projeto.")
+    print(f"Não foi possível atualizar o arquivo requirements, ambiente virtual '{nome_ambiente}' não encontrado na raiz do projeto.")
